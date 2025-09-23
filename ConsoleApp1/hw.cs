0a23c3111f849e89059683259844ac4c1c2aab17
liza631 <lizagoof890@gmail.com>
2025-09-23 21:27:54 +0800

    add fails

---
+++ ConsoleApp1/hw.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class hw
+    {
+        static void Main5(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1 || a == 2 || a == 12)
+            {
+                Console.WriteLine("Winter");
+            }
+            else if (a == 3 || a == 4 || a == 5)
+            {
+                Console.WriteLine("Spring");
+            }
+            else if (a == 6 || a == 7 || a == 8)
+            {
+                Console.WriteLine("Summer");
+            }
+            else if (a > 12 || a < 1)
+            {
+                Console.WriteLine("Ввели неправильное число");
+            }
+            else
+            {
+                Console.WriteLine("Autumn");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/mathquiz.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class mathquiz
+    {
+        static void Main3(string[] args)
+        {
+            int score = 0;
+
+
+            //создали объект который может генерировать рандомные числа
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                //rand.Next(min, max) - генерация числа от 10 до 100
+                int num1 = rand.Next(10, 100);
+                int num2 = rand.Next(10, 100);
+
+                Console.WriteLine($"{num1} + {num2} =");
+
+                int answer = Convert.ToInt32(Console.ReadLine());
+
+                if (answer == num1 + num2)
+                {
+                    Console.WriteLine("Ответ правильный!");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("Ответ не правильный");
+                    score--;
+                }
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
