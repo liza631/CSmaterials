54a109e11f586b58db13864bbfb2ea81688df03b
liza631 <lizagoof890@gmail.com>
2025-10-14 21:25:06 +0800

    rwrwer

---
+++ ConsoleApp3/array8.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class array8
+    {
+        static void Main(string[] args)
+        {
+            string filedString =
+                "#######" +
+                "#     #" +
+                "##  # #" +
+                "#   ###" +
+                "### # #" +
+                "#     #" +
+                "#######";
+
+            char[][] fieled = new char[7][];
+            fieled[0] = filedString.Substring(0, 7).ToCharArray();
+            fieled[1] = filedString.Substring(7, 7).ToCharArray();
+            fieled[2] = filedString.Substring(14, 7).ToCharArray();
+            fieled[3] = filedString.Substring(21, 7).ToCharArray();
+            fieled[4] = filedString.Substring(28, 7).ToCharArray();
+            fieled[5] = filedString.Substring(35, 7).ToCharArray();
+            fieled[6] = filedString.Substring(42, 7).ToCharArray();
+
+            int playerX = 3;
+            int playerY = 5;
+            while (true)
+            {
+                fieled[playerY][playerX] = '*';
+
+                for (int i = 0; i < fieled.Length; i++)
+                {
+                    for (int j = 0; j < fieled[i].Length; j++)
+                    {
+                        if (fieled[i][j] == ' ')
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                        }
+                        else if (fieled[i][j] == '#')
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                        }
+                        else if (fieled[i][j] == '*')
+                        {
+                            Console.BackgroundColor = ConsoleColor.Green;
+                        }
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.BackgroundColor = ConsoleColor.Black;
+
+                char direction = Console.ReadKey().KeyChar;
+
+                fieled[playerY][playerX] = ' ';
+                Console.Clear();
+
+                switch (direction)
+                {
+                    case 'd':
+                        if (fieled[playerY][playerX + 1] != '#')
+                        {
+                        playerX += 1;
+                        }
+                        break;
+                    case 'a':
+                        if (fieled[playerY][playerX - 1] != '#')
+                        {
+                            playerX -= 1;
+                        }
+                        break;
+                    case 'w':
+                        if (fieled[playerY - 1][playerX] != '#')
+                        {
+                            playerY -= 1;
+                        }
+                        break;
+                    case 's':
+                        if (fieled[playerY + 1][playerX] != '#')
+                        {
+                            playerY += 1;
+                        }
+                        break;
+                }
+            }
+;
+
+        }
+    }
+}
---
+++ ConsoleApp3/string1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class string1
+    {
+        static void Mainsfd(string[] args)
+        {
+            //1
+            /*
+            string[,] users = new string[10, 2];
+
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите фамилию: ");
+            string secondname = Console.ReadLine();
+
+            Console.Write("Введите пароль: ");
+            string password = Console.ReadLine();
+
+            string fullName = string.Concat(name, secondname); //объединить
+            fullName = fullName.Replace(" ", ""); //заменить
+            fullName = fullName.ToLower(); // преобразовать в нижний регистр
+            Console.WriteLine(fullName);
+
+            if (password.Contains(" ")) // есть ли элемент в строке
+            {
+                Console.WriteLine("Пароль не должен содержать пробел");
+            }
+            else
+            {
+                users[0,0] = fullName;
+                users[0,1] = password;
+                Console.WriteLine("Пользователь добавлен");
+            }
+
+            foreach (string data in users) { Console.WriteLine(data); }
+            */
+
+            //2
+            string[,] users = new string[2, 2] { { "вандех", "1234" }, { "ивандай", "4545" } };
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите фамилию: ");
+            string secondname = Console.ReadLine();
+
+            string fullname =string.Concat(name, secondname).ToLower().Replace(" ", "");
+
+            bool isUsers = true; 
+            foreach (string user in users)
+            {
+                if (user == fullname)
+                {
+                    isUsers = true;
+                    break;
+                }
+            }
+            if (isUsers)
+            {
+                Console.WriteLine("Пользователь найден");
+                Console.Write("Введите пароль: ");
+                string password = Console.ReadLine();
+
+                for (int i = 0; i < users.Length / 2; i++)
+                {
+                    if (users[i, 0] == fullname && users[i, 1] == password)
+                    {
+                        Console.WriteLine("Пользователь удалён");
+                        users[i, 0] = string.Empty;
+                        users[i, 1] = string.Empty;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Полбзователь не найден");
+            }
+            foreach (string data in users)
+            {
+                Console.WriteLine(data);
+            }
+            
+
+            
+
+
+        }
+    }
+}
